838533177b3b39352e1a58fca162d715af466fee
joe-trung <kieutrungtran@gmail.com>
2023-03-04 16:02:05 -0500

    First Amendment

--- src/main/java/WriteLoops.java
+++ src/main/java/WriteLoops.java
@@ -15,6 +15,9 @@ public class WriteLoops {
 
     public int oneToFive() {
         int w = 0;
+        for (int i=1;i<=10;i++) {
+            w = w + 1;
+        }
 
         // Write a FOR loop that counts from 1 to 10.
             // calling
@@ -27,6 +30,9 @@ public class WriteLoops {
 
     public int oneToTen() {
         int w = 0;
+        for (int i = 1;i<=10; i++) {
+            w = w + 1;
+        }
 
         // Write a FOR loop that counts from 1 to 10.
         // calling
@@ -38,6 +44,9 @@ public class WriteLoops {
 
     public int startAtTwentyOne() {
         int w = 0;
+        for (int i = 21; i<=30; i++) {
+            w = w + 1;
+        }
 
         // Write a FOR loop that makes 10 iterations, start at 21.
         // calling
@@ -49,6 +58,9 @@ public class WriteLoops {
 
     public int countDown() {
         int w = 0;
+        for (int i=100; i>=0; i--) {
+            w = w + 1;
+        }
 
         // Write a FOR loop that counts down from 100 to 0.
         // calling
@@ -60,6 +72,9 @@ public class WriteLoops {
 
     public int byTwoTo32() {
         int w = 0;
+        for (int i = 0; i<=32; i = i+2) {
+            w = w + 1;
+        }
 
         // Write a FOR loop from 0 to 32 by 2s.
         // calling
@@ -70,6 +85,7 @@ public class WriteLoops {
 
     public int countDownFrom5000() {
         int w = 0;
+        for (int i=1; i<=5001; i=i+11)
 
         // Write a FOR loop from 1 to less than 5001 by 11s.
         // calling
@@ -81,6 +97,11 @@ public class WriteLoops {
 
     public int nestedFors() {
         int w = 0;
+        for (int i=0; i<=20;i++) {
+            for (int j = 0; j<=4; j++) {
+                w = w + 1;
+            }
+        }
 
         // Write a nested FOR loop(s), where one counts from
         // 0 to less than 20 and the inner one counts from 0 to 4
@@ -93,6 +114,14 @@ public class WriteLoops {
 
     public int helloZipCode() {
         int w = 0;
+        for (int i=5; i<=105;i++) {
+            if (i>51) {
+                System.out.println("Hello Zipcode");
+            }
+            else {
+                w = w + 1;
+            }
+        }
 
         // Write a FOR loop that counts from 5 to 105. Put an IF
         // statement inside the loop that checks the
@@ -129,14 +158,16 @@ public class WriteLoops {
     // After the loop is done, print “Honey, I’m Home!”
     public int driveHome() {
         int w = 0;
+        while (!(gpsCurrentLocation().equals("Home"))) {
+            driveSomeMore();
 
-        // you need to use a .equals for two Strings.
+            // you need to use a .equals for two Strings.
 
             // calling
             w = w + 1;
             // each time through the inner loop
-        
-
+        }
+            System.out.println("Honey, I'm Home!");
             return w;
     }
 
@@ -151,13 +182,17 @@ public class WriteLoops {
         int highestScore = 236;
         int currentScore = gameNextScore();
         int runningScore = 0;
+        while (runningScore < highestScore) {
+            runningScore = runningScore + currentScore;
+            currentScore = gameNextScore();
 
-        // do your while loop here
- 
+
+            // do your while loop here
+
             // calling
             w = w + 1;
             // each time through the inner loop
-        
+        }
         return w; // >= 3;
     }
 
@@ -170,10 +205,15 @@ public class WriteLoops {
         int runningScore = 0;
 
         // do your while loop here
+        do {
+            runningScore = runningScore + currentScore;
+            currentScore = gameNextScore();
+
 
             // calling
             w = w + 1;
             // each time through the inner loop
+        } while (runningScore < highestScore)
 
         return w >= 3;
     }
